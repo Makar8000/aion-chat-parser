16e79d673c67e0ea429162b55b48ef1fb2662594
Makar <Makar8000@users.noreply.github.com>
2024-02-13 17:42:15 -0600

    update custom items

--- processors/index.js
+++ processors/index.js
@@ -1,3 +1,5 @@
+const fs = require('fs');
+const path = require('path');
 const lfgProcessor = require('./lfg');
 const items = require('../data/items.json');
 const itemsOverride = require('../data/items-custom.json');
@@ -13,20 +15,56 @@ const REG = {
 };
 
 const processLine = async (line) => {
-  const parsedLine = parseAionMessage(line);
+  const parsedLine = await parseAionMessage(line);
   lfgProcessor(parsedLine);
 };
 
-const parseAionMessage = msg => {
+const parseAionMessage = async (msg) => {
   let ret = msg.substring(22);
   REG.COMMON.forEach(r => {
     ret = ret.replaceAll(r.regex, r.replace);
   });
   for (const match of ret.matchAll(REG.ITEM)) {
     const id = match?.groups?.itemId;
+    if (!items[id]?.markupLink) {
+      await parseUnknownItem(id);
+    }
     const markup = items[id]?.markupLink ?? `[<UnknownItem ${id}>](https://aioncodex.com/usc/item/${id}/)`;
     ret = ret.replaceAll(match?.groups?.item, markup);
   }
+  return ret;
+};
+
+const parseUnknownItem = async itemId => {
+  const ret = {
+    id: Number.parseInt(itemId),
+    url: `https://aioncodex.com/usc/item/${itemId}/`,
+    name: `UnknownItem ${itemId}`,
+    markupLink: `[<UnknownItem ${itemId}>](https://aioncodex.com/usc/item/${itemId}/)`,
+  };
+
+  try {
+    await fetch(`https://aioncodex.com/usc/item/${itemId}/`)
+      .then(res => res.text())
+      .then(body => {
+        // Parse title from response
+        const match = body.match(/<title>(?<title>[^<]*)<\/title>/);
+        if (typeof match?.groups?.title !== 'string')
+          return;
+
+        ret.name = match.groups.title.replace(" - Aion Codex", "").trim();
+        ret.markupLink = `[<${ret.name}>](https://aioncodex.com/usc/item/${itemId}/)`;
+
+        // Update override items
+        itemsOverride[`${itemId}`] = ret;
+        items[`${itemId}`] = ret;
+        fs.writeFileSync(path.join(__dirname, '../data/items-custom.json'), JSON.stringify(itemsOverride, null, 2));
+        return;
+      });
+  } catch (e) {
+    console.error(e);
+  }
+
   return ret;
 };
 
